8338e89a28b3d1a35bd67a28311f20cda0118482
cihan <kurbancihan@gmail.com>
2018-12-01 00:51:59 +0300

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,16 +1,27 @@
 public static void main(String[] args){
-int input;
-int c;
-int a = 0;
-int b = 1;
-Scanner deger = new Scanner(System.in);
-System.out.println("Bir sayı giriniz");
-input = deger.nextInt();
-for(int i =0;i <input;i++){
-c = a+ b;
-a = b;
-b = c;
-System.out.println(" " + a );
+  int input;
+  int c;
+  int a = 0;
+  int b = 1;
+  Scanner deger = new Scanner(System.in);
+  System.out.println("Bir sayı giriniz");
+  input = deger.nextInt();
+  for(int i =0;i <input;i++){
+    c = a+ b;
+    a = b;
+    b = c;
+    System.out.println(": " + a );
 }
-deger.close();
+  deger.close();
 }
+  
+
+
+
+
+
+
+
+
+
+
